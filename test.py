89759ed51c7879cfb368da8666c9bd946500140d
tclin08 <tclin08@163.com>
2017-08-14 21:24:40 +0800

    test
    test

---
+++ test.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+
+print('input a No')
+spam = input()
+spam = int(spam)
+if spam == 1:
+	print('hello')
+elif spam == 2:
+	print('howdy')
+else :
+	print('greeting')
+#i = 0
+#for i in range(1,11):
+#	print(i)
+#i = 1
+#while (i < 11):
+#	print(i)
+#	i = i+1
